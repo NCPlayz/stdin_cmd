d442ee7e22beafe9e7bc7d7065e19382c15564f1
Nadir Chowdhury <chowdhurynadir0@outlook.com>
2017-12-01 19:50:26 +0000

    Changed implementation - makes all ints to floats.

--- stdin_cmd/main.py
+++ stdin_cmd/main.py
@@ -1,28 +1,35 @@
 from stdin_cmd.commands import command, exec_command
-from stdin_cmd.utils import make_number
 
 
 @command(args=2, usage='add <arg1: Integer or Float> <arg2: Integer or Float>')
 def add(arg1, arg2):
     """Adds two numbers together."""
-    arg1 = make_number(arg1)
-    arg2 = make_number(arg2)
+    arg1 = float(arg1)
+    arg2 = float(arg2)
     print(f"Your answer is {arg1 + arg2}.")
+
+
+@command(args=2, usage='minus <arg1: Integer or Float> <arg2: Integer or Float>')
+def minus(arg1, arg2):
+    """Adds two numbers together."""
+    arg1 = float(arg1)
+    arg2 = float(arg2)
+    print(f"Your answer is {arg1 - arg2}.")
 
 
 @command(args=2, usage='multiply <arg1: Integer or Float> <arg2: Integer or Float>')
 def multiply(arg1, arg2):
     """Multiplies two numbers by each other."""
-    arg1 = make_number(arg1)
-    arg2 = make_number(arg2)
+    arg1 = float(arg1)
+    arg2 = float(arg2)
     print(f"Your answer is {arg1 * arg2}.")
 
 
 @command(args=2, usage='divide <arg1: Integer or Float> <arg2: Integer or Float>')
 def divide(arg1, arg2):
     """Divides one number by another."""
-    arg1 = make_number(arg1)
-    arg2 = make_number(arg2)
+    arg1 = float(arg1)
+    arg2 = float(arg2)
     print(f"Your answer is {arg1 / arg2}.")
 
 
